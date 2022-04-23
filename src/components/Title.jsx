f917a2d700044795eed530e8988387d87f44dfbc
Mohamed <farahmo242@gmail.com>
2022-04-22 22:27:40 -0400

    Quick photography portfolio

---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>MAF Portfolio</h1>
+      <h2>Hobby Shots</h2>
+      <p>i do this for fun.</p>
+    </div>
+  );
+};
+
+export default Title;
---
+++ src/image.js
@@ -0,0 +1,40 @@
+const IMAGES = [
+  {
+    id: 1,
+    image: "/images/1.JPG",
+  },
+  {
+    id: 3,
+    image: "/images/3.JPG",
+  },
+  {
+    id: 8,
+    image: "/images/8.JPG",
+  },
+  {
+    id: 4,
+    image: "/images/4.JPG",
+  },
+  {
+    id: 6,
+    image: "/images/6.JPG",
+  },
+  {
+    id: 6,
+    image: "/images/7.JPG",
+  },
+  {
+    id: 5,
+    image: "/images/5.JPG",
+  },
+
+  {
+    id: 11,
+    image: "/images/11.JPG",
+  },
+  {
+    id: 12,
+    image: "/images/12.JPG",
+  },
+];
+export default IMAGES;
